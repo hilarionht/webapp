b8f546b06adafe2478ac8380f9058b8caccf7c1f
hilarionht <trejo.hilarion@gmail.com>
2020-04-07 13:37:19 -0300

    dist cliente

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const expess = require('express');
 const app = expess();
-app.get('/', (req, res) => res.send('Hello world!'));
+const path = require('path');
+app.use('/', expess.static(path.join(__dirname, 'dist')));
+// app.get('/', (req, res) => res.send('Hello world!'));
+app.use('*', function(req, res, next) {
+    res.sendFile(path.resolve('dist/index.html'));
+});
 app.listen(3000);
+
 console.log('server on port ', 3000);
+//module.exports = app;
